e845a0cfde62774012d52940e24b54a592b5cae8
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-05 17:56:35 -0600

    Agregando el listar mascotas

--- facilito/facilito/apps/mascotas/views.py
+++ facilito/facilito/apps/mascotas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from facilito.apps.mascotas.forms import MascotaForm
+from facilito.apps.mascotas.models import Mascota
 
 def index(request):
     return render(request, "mascotas/index.html")
@@ -17,3 +18,10 @@ def mascota_view(request):
         form = MascotaForm()
 
     return render(request, 'mascotas/mascota_form.html', {'form': form})
+
+
+def mascota_list(request):
+    mascotas = Mascota.objects.all()
+    context = {'mascotas': mascotas}
+
+    return render(request, 'mascotas/mascota_list.html', context)
