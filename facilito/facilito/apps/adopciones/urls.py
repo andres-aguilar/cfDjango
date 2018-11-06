a075c293d447c389c8e0351b83c50988c8574ac7
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-06 13:42:12 -0600

    Agregando CRUD con vistas basadas en clases para las adopciones

--- facilito/facilito/apps/adopciones/urls.py
+++ facilito/facilito/apps/adopciones/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from facilito.apps.adopciones.views import index
+from facilito.apps.adopciones.views import index, SolicitudList, SolicitudCreate, SolicitudUpdate, SolicitudDelete
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^listar', SolicitudList.as_view(), name='solicitud_listar'),
+    url(r'^crear', SolicitudCreate.as_view(), name='solicitud_crear'),
+    url(r'^editar/(?P<pk>\d+)/$', SolicitudUpdate.as_view(), name='solicitud_editar'),
+    url(r'^eliminar/(?P<pk>\d+)/$', SolicitudDelete.as_view(), name='solicitud_eliminar')
 ]
