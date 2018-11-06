749926b719060d9a80378706ca1cd215d7dd1cca
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-06 16:57:04 -0600

    Agregnado opcion para reestablecer contraseña por correo

--- facilito/facilito/settings.py
+++ facilito/facilito/settings.py
@@ -133,3 +133,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
 
 LOGIN_REDIRECT_URL = reverse_lazy('adopciones:solicitud_listar')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
+
+# Envio de correos 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mail@gmail.com'
+EMAIL_HOST_PASSWORD = 'supersecretpassword'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
