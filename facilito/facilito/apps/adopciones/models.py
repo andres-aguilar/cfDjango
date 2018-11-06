f067d1fc8d75a8147dd6860d392c913dcf5f19b0
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-06 13:15:27 -0600

    Agregando modelo y migraciones para las adopciones

--- facilito/facilito/apps/adopciones/models.py
+++ facilito/facilito/apps/adopciones/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Persona(models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=70)
@@ -11,3 +11,9 @@ class Persona(models.Model):
 
     def __str__(self):
         return self.nombre + " " + self.apellidos
+
+
+class Solicitud(models.Model):
+    persona = models.ForeignKey(Persona, null=True, blank=True)
+    numero_mascotas = models.IntegerField()
+    razones = models.TextField()
