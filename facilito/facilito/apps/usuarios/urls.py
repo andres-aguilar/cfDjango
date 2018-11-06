a2030d4ba4b7e49da5db5537201b3d7396640437
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-06 17:29:16 -0600

    Ejemplo de serialización con Django RestFramework

--- facilito/facilito/apps/usuarios/urls.py
+++ facilito/facilito/apps/usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from facilito.apps.usuarios.views import RegistroUsuario
+from facilito.apps.usuarios.views import RegistroUsuario, UserAPI
 
 urlpatterns = [
     url(r'^registrar/$', RegistroUsuario.as_view(), name='registro'),
+    url(r'^api/$', UserAPI.as_view(), name='api'),
 ]
--- facilito/facilito/apps/usuarios/views.py
+++ facilito/facilito/apps/usuarios/views.py
@@ -1,10 +1,16 @@
+import json
+
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 
+from rest_framework.views import APIView
+
 from facilito.apps.usuarios.forms import RegistroForm
+from facilito.apps.usuarios.serializers import UserSerializer
 
 
 class RegistroUsuario(CreateView):
@@ -12,3 +18,13 @@ class RegistroUsuario(CreateView):
     template_name = 'usuarios/registrar.html'
     form_class = RegistroForm
     success_url = reverse_lazy('mascotas:mascota_listar')
+
+
+class UserAPI(APIView):
+    serializer = UserSerializer
+
+    def get(self, request, format=None):
+        lista = User.objects.all()
+        response = self.serializer(lista, many=True)
+
+        return HttpResponse(json.dumps(response.data), content_type='application/json')
